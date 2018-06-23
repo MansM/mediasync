495f78a9c2e93579c2a971f455ff17dbaa72b2bb
Mans Matulewicz <mans.matulewicz@gmail.com>
2018-06-23 15:25:22 +0200

    http calls counter added

--- mediasync/poolmanager.py
+++ mediasync/poolmanager.py
@@ -3,11 +3,13 @@ import urllib3
 class PoolManager(object):
   def __init__(self):
     self.http = urllib3.PoolManager()
+    self.counter=0
 
   def __del__(self):
-    print ("PoolManager closed")
+    print ("PoolManager closed, httpcounter: " + str(self.counter))
 
   def request(self, method, url, body=None, headers=None):
+    self.counter+=1
     return self.http.request(method, url, body=body, headers=headers)    
 
 poolmanager = PoolManager()
