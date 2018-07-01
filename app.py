c39143b4743d3d2fa14df59283a498acd3a9c27d
Mans Matulewicz <mans.matulewicz@gmail.com>
2018-07-01 09:44:01 +0200

    support for kodi credentials

--- app.py
+++ app.py
@@ -3,19 +3,20 @@ from mediasync import Plex
 from mediasync import Kodi
 from mediasync import Settings
 from mediasync.Logger import logger
-#from mediasync.poolmanager import poolmanager
+from mediasync.poolmanager import poolmanager
 
 
-#import yaml, logging, base64
+import yaml, logging, base64
 
 def main():
 
   settings = Settings()
   
   logger.configure(settings.logfile, settings.loglevel)
-  settings.initMedia()
-  
   logger.info("Log started")
+  
+  settings.initMedia()
+
 
   for plex in settings.plexes:
     plex.backupShows()
@@ -33,20 +34,7 @@ def main():
     kodi.restoreShows()
     kodi.restoreMovies()
 
-  #poolmanager.http.headers["user"] = "kodi"
-  # raw = bytes("%s:%s"%("kodi", str(1)), "UTF-8")
-  # #print(raw)
-  # auth = "Basic %s"  % base64.b64encode(raw).strip().decode("utf-8")
-  # poolmanager.http.headers["Authorization"] = auth
-  # r = poolmanager.request('POST', "http://10.0.1.53:8080/jsonrpc", body=None, headers={"Content-Type": "application/json", "Authorization": auth})
-
-  # print(r.status)
   logger.debug("log end")
- 
-  # plex.restoreMovies()
-  # plex.restoreShows()
-  # kodi.restoreShows()
-
 
 if __name__ == "__main__":
   main()
